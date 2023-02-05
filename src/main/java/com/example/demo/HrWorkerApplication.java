56092766c3a8fdb9bfa1ed53f875a829e170205a
daniel <danielsilvacr123@gmail.com>
2023-02-05 16:07:04 -0300

    hr-worker implementation

--- src/main/java/com/example/demo/HrWorkerApplication.java
+++ src/main/java/com/example/demo/HrWorkerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HrWorker1Application {
+public class HrWorkerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HrWorker1Application.class, args);
+		SpringApplication.run(HrWorkerApplication.class, args);
 	}
 
 }
